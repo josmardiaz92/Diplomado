a9c1cba0d1b5ed8f6b2fc03d0b1b5bd95579e654
josmardiaz92 <josmardiaz92@gmail.com>
2023-07-15 10:33:20 -0400

    acomodo de tarjetas

--- claseJs/apis/js/marvel.js
+++ claseJs/apis/js/marvel.js
@@ -1,21 +1,30 @@
 const urlAPI="https://gateway.marvel.com:443/v1/public/characters?ts=1689204428433&apikey=f4a0f8c6d2dfc289a81abbcebc496e21&hash=fdbe7046ba2d431a2cbbf5b11be62df9";
 let contenHTML='';
+let contenTextCard='';
 fetch(urlAPI)
     .then(res=>res.json())
     .then(json=>{
         console.log(json)
-        json.data.results.forEach(hero => {
+        json.data.results.forEach((hero, index) => {
             contenHTML+=`
             <div class="card col-3 m-3">
                 <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" class="card-img-top" alt="${hero.name}">
                 <div class="card-body">
                 <h5 class="card-title">${hero.name}</h5>
-                <p class="card-text">Esta es una tarjeta más amplia con texto de apoyo a continuación como introducción natural a contenido adicional. Este contenido es un poco más largo.</p>
-                <p class="card-text"><small class="text-muted">Última actualización hace 3 minutos</small></p>
+                <p class="card-text text-dark" id="aparicion${index}">Aparece en las series:<br></p>
+                <p class="card-text"><a class="text-danger text-decoration-none" href="${hero.urls[0].url}" target="_blank">Ver mas</a></p>
                 </div>
             </div>`
         });
         marvelCard.innerHTML=contenHTML;
+        json.data.results.forEach((hero, index)=>{
+            contenTextCard='';
+            hero.series.items.forEach((series, index)=>{
+                contenTextCard+=` ${series.name}<br>`
+            })
+            let idCard=document.getElementById(`aparicion${index}`);
+            idCard.innerHTML=contenTextCard;
+        })
 
     })
 .catch(error =>{console.error(error)})
