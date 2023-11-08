2d7e28ccee10d9136aa4911825e15bd1f302f59d
josmardiaz92 <josmardiaz92@gmail.com>
2023-11-08 19:52:15 -0400

    bases

--- aprendiendo react/diplomado_react/react_app/src/index.js
+++ aprendiendo react/diplomado_react/react_app/src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>¡Hola Mundo!</h1>
   </React.StrictMode>
-);
+); */
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const nombre='Clase';
+const color='blue';
+const sesion=true;
+const clase='Diplomado de Programación';
+const instructores=['Pedro','Maria Eugenia','Vidermid','Gabriel'];
+const JSX=(
+  <>
+    {sesion===true ?
+    <>
+      <h1 style={{color:color}} >¡Hola {nombre}!</h1> {/* Asi se llaman las variables dentro de JSX */}
+      <p>Te doy la bienvenida a esta Experiencia de Aprendizaje</p>
+      {clase && <p>Estamos en clase del {clase}</p>} {/* en caso de estar definida, se muestra, es similar al terniario */}
+      <h4>Estos son los Profesores:</h4>
+      <ul>
+        {instructores.map((instructor,index)=>{
+          return <li key={index}>{instructor}</li>
+        })}
+      </ul>
+    </>
+    :
+    <h1>No Has Iniciado Sesion</h1>
+    }
+  </>
+);
+const verificarSesion=(sesion)=>{
+  if(sesion===true){
+    return JSX;
+  }else{
+    return <h1>No Has Iniciado Sesion</h1>
+  }
+}
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(JSX)
