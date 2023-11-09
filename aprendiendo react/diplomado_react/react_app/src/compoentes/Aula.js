ba30fa69e1d8c516e6cbb0ebc0f5f21e41875b06
josmardiaz92 <josmardiaz92@gmail.com>
2023-11-09 19:17:53 -0400

    fin de la clase 2

--- aprendiendo react/diplomado_react/react_app/src/compoentes/Aula.js
+++ aprendiendo react/diplomado_react/react_app/src/compoentes/Aula.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { TituloAzul,TituloNaranja } from "./Titulo";
+import Titulo from "./Titulo";
 const Aula=()=>{
     const clase='Diplomado de Programación';
     const instructores=['Pedro','Maria Eugenia','Vidermid','Gabriel'];
     return(
         <>
-            <TituloAzul/>
-            <TituloNaranja/>
+            <Titulo nombre='Josmar' color='blue'/>
+            <Titulo nombre='Alejandro' color='orange'/>
+            <Titulo nombre='Enrrique' color='green'/>
             <p>Te doy la bienvenida a esta Experiencia de Aprendizaje</p>
             {clase && <p>Estamos en clase del {clase}</p>}
             <h4>Estos son los Profesores:</h4>
--- aprendiendo react/diplomado_react/react_app/src/compoentes/Titulo.js
+++ aprendiendo react/diplomado_react/react_app/src/compoentes/Titulo.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-const TituloAzul=()=>{
-    const nombre='Clase';
-    const color='blue';
+/* esta es una forma de heredar */
+/* const TituloAzul=(props)=>{ 
+    return(
+        <>
+            <h1 style={{color:props.color}} >¡Hola {props.nombre}!</h1>
+        </>
+    );
+}; */
+
+const Titulo=({nombre,color})=>{
     return(
         <>
             <h1 style={{color:color}} >¡Hola {nombre}!</h1>
@@ -10,14 +17,4 @@ const TituloAzul=()=>{
     );
 };
 
-const TituloNaranja=()=>{
-    const nombre='Clase';
-    const color='orange';
-    return(
-        <>
-            <h1 style={{color:color}} >¡Hola {nombre}!</h1>
-        </>
-    );
-};
-
-export {TituloAzul,TituloNaranja};
+export default Titulo;
